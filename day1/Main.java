769788b6122960cd744882aa63e654f3fb34aa4d
Katsuki Ishii <141katsuki141@gmail.com>
2026-07-26 20:37:23 +0900

    branch add

--- day1/Main.java
+++ day1/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Git practice");
+        System.out.println("feature-messageで追加");
     }
 }
